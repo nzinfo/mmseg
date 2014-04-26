e40f7edff330651c56c1cdf9c70e40922e9e15d6
nzinfo <limn@coreseek.com>
2014-04-26 21:21:57 +0800

    fix cli

--- src/mmseg_cli.cpp
+++ src/mmseg_cli.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <map>
 #include  <stdlib.h>
+#include <stdio.h>
 
 #ifdef WIN32
 #include "bsd_getopt_win.h"
@@ -16,8 +17,62 @@
 #include "bsd_getopt.h"
 #endif
 
+#define MMSEG_DEBUG 1
+
+// mmseg includes
+#include "segmentor.h"
+
+void usage(const char* argv_0) {
+    printf("Coreseek COS(tm) MMSeg 2.0\n");
+    printf("Copyright By Coreseek.com All Right Reserved.\n");
+    printf("Usage: %s <option> <file>\n",argv_0);
+    printf("-d <dict_path>           Dictionary\n");
+    printf("-h            print this help and exit\n");
+    return;
+}
+
+
 int main(int argc, char **argv) {
-    printf("hello");
+
+    char resolved_dict_path[255];
+    int c;
+    const char* dict_path = NULL;
+    const char* out_file = NULL;
+    u1 bQuite = 0;
+
+    if(argc < 2){
+        usage(argv[0]);
+        exit(0);
+    }
+
+    while ((c = getopt(argc, argv, "d:q")) != -1) {
+        switch (c) {
+        case 'd':
+            dict_path = optarg;
+            break;
+        case 'q':
+            bQuite = 1;
+            break;
+        case 'h':
+            usage(argv[0]);
+            exit(0);
+        default:
+            fprintf(stderr, "Illegal argument \"%c\"\n", c);
+            return 1;
+        }
+    }
+
+    if(optind < argc) {
+        out_file = argv[optind];
+    }
+
+    realpath(dict_path, resolved_dict_path);
+
+#if MMSEG_DEBUG
+    printf("dict=%s; file=%s\n", resolved_dict_path, out_file);
+#endif
+
+
     return 0;
 }
 
