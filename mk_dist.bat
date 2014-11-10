b5d360a302a498b36be66deaa59b12896017fc43
nzinfo <limn@coreseek.com>
2014-11-10 14:02:08 +0800

    change win32 build param

--- mk_dist.bat
+++ mk_dist.bat
@@ -14,8 +14,8 @@ md libcss\lib\release
 md libcss\lib\x64\debug
 md libcss\lib\x64\release
 
-copy win32\release\*.lib libcss\lib\release\
-copy win32\debug\*.lib libcss\lib\debug\
+copy win32\release\libcss.lib libcss\lib\libcss.lib
+copy win32\debug\libcss.lib libcss\lib\libcss_d.lib
 
 copy win32\x64\release\*.lib libcss\lib\x64\release\
 copy win32\x64\debug\*.lib libcss\lib\x64\debug\
